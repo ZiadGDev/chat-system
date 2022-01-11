5bd94c87630f215dbed643deb0602d9b10a4dd07
Ziad Ayman <ziad.ayman1993@gmail.com>
2022-01-11 21:45:37 +0200

    Optimizing chats API

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,23 +1,17 @@
 class ChatsController < ApplicationController
     def index
-        @app = App.find_by(token: params[:app_token])
-        @chats = @app.nil? ? [] : @app.chat
+        @chats = Chat.joins(:app).where('apps.token': params[:app_token])
         render json: @chats
     end
     def create
         @app = App.lock.find_by(token: params[:app_token])
-        if (@app.nil?)
-            return
-        end
-
         @app.chats_count += 1
         @chat = Chat.create(number: @app.chats_count)
         @app.chat << @chat
         @app.save
     end
     def show
-        @app = App.find_by(token: params[:app_token])
-        @chat = @app.nil? ? nil : @app.chat.find_by(number: params[:number])
+        @chat = Chat.joins(:app).where('app.token': params[:app_token], number: params[:number])[0]
         render json: @chat
     end
 end
